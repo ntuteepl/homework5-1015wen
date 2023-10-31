bce748a364d1f15ab8c9d00b3406a786c66753e2
1015wen <146065580+1015wen@users.noreply.github.com>
2023-10-31 15:04:11 +0800

    Update hw5-2.c

--- hw5-2.c
+++ hw5-2.c
@@ -1,1 +1,30 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float a;
+    double c;
+
+    scanf("%f",&a);
+
+    if(a<=800)
+    {
+        c=a*0.9;
+        printf("%.1f",c);
+    }
+    if(800<a<1500)
+    {
+        c=a*0.81;
+        printf("%.1f",c);
+    }
+    if(a>=1500)
+    {
+        c=a*0.79;
+        printf("%.1f",c);
+    }
+
+
+    return 0;
+}
