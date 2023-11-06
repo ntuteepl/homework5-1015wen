99a813ef6f9c751e7c7216b8609601bb4d1e3279
1015wen <146065580+1015wen@users.noreply.github.com>
2023-11-06 23:39:40 +0800

    Update hw5-4.c

--- hw5-4.c
+++ hw5-4.c
@@ -1,1 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    float h,m;
+    double c,d,e;
+
+    scanf("%f%f",&h,&m);//h小時m分鐘
+    if(h>=1&&h<=12&&m>=0&&m<=59)
+    {
+        d=m*6;
+        if(m>=0&&m<=12)
+        {
+            c=h*30+6*(m/12);
+        }
+        if(m>12&&m<=24)
+        {
+            c=h*30+6*(m-12)/12;
+        }
+        if(m>24&&m<=36)
+        {
+            c=h*30+6*(m-24)/12;
+        }
+        if(m>36&&m<=48)
+        {
+            c=h*30+6*(m-36)/12;
+        }
+        if(m>48&&m<=59)
+        {
+            c=h*30+6*(m-48)/12;
+        }
+        //e=d-c;
+        if(d>c)
+        {
+            e=d-c;
+            if(e>180)
+            {
+                e=360-e;
+                printf("%.3f",e);
+            }
+            else
+            {
+                printf("%.3f",e);
+            }
+        }
+        if(d<c)
+        {
+            e=c-d;
+            if(e>180)
+            {
+            e=360-e;
+            printf("%.3f",e);
+            }
+            else
+            {
+                printf("%.3f",e);
+            }
+        }
+        
+    }
+    
+
+    return 0;
+}
