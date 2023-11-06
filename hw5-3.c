db2983f3231fc67cfe3e8e9ce50c0803ae29036c
1015wen <146065580+1015wen@users.noreply.github.com>
2023-11-06 21:40:17 +0800

    Update hw5-3.c

--- hw5-3.c
+++ hw5-3.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    float a,s;
+    double c;
+    scanf("%f%f",&a,&s);
+    printf("%.0f %.0f\n",a,s);
+    if(a<=60)
+    {
+        c=a*s;
+        printf("%.1f\n",c);
+    }
+    if(a>60&&a<=120)
+    {
+        c=60*s+(a-60)*s*1.33;
+        printf("%.1f\n",c);
+    }
+    if(a>120)
+    {
+        c=60*s+60*s*1.33+(a-120)*s*1.66;
+        printf("%.1f\n",c);
+    }
+
+    return 0;
+}
 
